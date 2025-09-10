5a02bb85538360c5010089d006485950fbe03dc4
Ishwari-4 <ishwarinaik2003@gmail.com>
2025-09-10 20:14:15 +0530

    Update models.py

--- L3. Azure Storage/Connecting_to_your_App_to_Storage/FlaskExercise/models.py
+++ L3. Azure Storage/Connecting_to_your_App_to_Storage/FlaskExercise/models.py
@@ -4,9 +4,9 @@ from werkzeug.utils import secure_filename
 from azure.storage.blob import BlobServiceClient
 import uuid
 
-blob_container = app.config['BLOB_CONTAINER']
-storage_url = "https://{}.blob.core.windows.net/".format(app.config['BLOB_ACCOUNT'])
-blob_service = BlobServiceClient(account_url=storage_url, credential=app.config['BLOB_STORAGE_KEY'])
+blob_container = app.config['images']
+storage_url = "https://{}.blob.core.windows.net/".format(app.config['helloworld12345'])
+blob_service = BlobServiceClient(account_url=storage_url, credential=app.config['/6sf0QPfSsI/Ml3dfudx6sjQGFAKIawLZWW4JTgBFYBlaEcLVvzls73JROgsn/QOW54frnWRQWz7+AStWH2VMA=='])
 
 class Animal(db.Model):
     __tablename__ = 'animals'
@@ -26,10 +26,12 @@ class Animal(db.Model):
             randomFilename = str(uuid.uuid1())
             filename = randomFilename + '.' + fileExtension
             try:
-                # TODO: Get a blob client and upload the blob
+                blob_client = blob_service.get_blob_client(container=blob_container, blob=filename)
+blob_client.upload_blob(file)
                 pass
                 if self.image_path:
-                    # TODO: Get a blob client and delete the previous blob
+                    blob_client = blob_service.get_blob_client(container=blob_container, blob=self.image_path)
+blob_client.delete_blob()
                     pass
             except Exception as err:
                 flash(err)
